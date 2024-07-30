0a885babf5dc7fa51892d90b224ad6729910a86f
abhinav3254 <abhinav3254@gmail.com>
2024-07-30 12:31:47 +0530

    multer file upload

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 const { port } = require('./config/config');
 
+// multer file upload import
+const upload = require('./middlewares/upload');
+
 app.get('', (req, res) => {
     return res.status(200).json({ message: 'Hola!' });
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+
+    if (!req.file) {
+        return res.status(400).json({ message: 'File not found' });
+    }
+
+    return res.status(200).json({ message: 'File uploaded!' });
+
 });
 
 app.listen(port, () => {
